939687eac689681bbff76d139ba807bbe369f738
Jintime <wlstjd7391@gmail.com>
2023-11-16 16:55:41 +0900

    컬럼테이블추가

---
+++ src/components/table/columTable.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="table-container">
+    <table>
+      <tbody>
+      <tr v-for="(header, index) in headers" :key="index">
+        <th>{{ header }}</th>
+        <td v-for="row in tableData" :key="row[header]">{{ row[header] }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ColumTable',
+  props: {
+    headers: {
+      type: Array,
+      required: true
+    },
+    tableData: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
