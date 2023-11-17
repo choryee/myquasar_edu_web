8fa17b54d2f2fafba9951343305ace4f728ebb39
김병준 <kbj@emgram.kr>
2023-11-17 14:22:50 +0900

    [feature/#10] 연차목록 페이지 클릭 이벤트 추가

---
+++ src/components/table/ClickableTable.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="table-container">
+    <table>
+      <thead>
+      <tr>
+        <th v-for="header in headers" :key="header">{{ header }}</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(row, index) in tableData" :key="index" class="clickable" @click="clickEvent(clickAbleEventKeys[index])">
+        <td v-for="(value, key) in row" :key="key">{{ value }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ClickableRowTable',
+  props: {
+    headers: {
+      type: Array,
+      required: true
+    },
+    tableData: {
+      type: Array,
+      required: true
+    },
+    isClickable: {
+      type: Boolean,
+    },
+    clickAbleEventKeys:{
+      type: Array,
+    }
+  },
+  methods:{
+    clickEvent(key) {
+      console.log(this.clickAbleEventKeys);
+      this.$emit("clickRow", key);
+    }
+  }
+};
+</script>
+
+
