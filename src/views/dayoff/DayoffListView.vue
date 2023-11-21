ff2bdaeb5ffa88e0b47ce1fe04863979fd4b4eb1
김병준 <kbj@emgram.kr>
2023-11-21 12:19:16 +0900

    [feature/#17] 캘린더-연차목록간 이동 버튼 개발

--- src/views/dayoff/DayoffListView.vue
+++ src/views/dayoff/DayoffListView.vue
@@ -1,27 +1,45 @@
 <template>
+  <div class="calendar-change-button-container">
+    <button class="calendar-change-button left-button" @click="changeCalendar" :class="isCalendarView ? 'button-active' : ''">
+      캘린더
+    </button>
+    <button class="calendar-change-button right-button" @click="changeList" :class="!isCalendarView ? 'button-active' : ''">
+      연차 목록
+    </button>
+  </div>
   <div class="content-header">
-    <h1>
+    <h1 v-if="!isCalendarView">
       사원별 잔여 연차 조회
     </h1>
-    <div id="calendar-change-button-container">
-      <div class="calendar-change-button button-active">
-
-      </div>
-      <div class="calendar-change-button">
-
-      </div>
-    </div>
   </div>
-  <DayoffListTable/>
+  <template v-if="isCalendarView">
+    <Calendar/>
+  </template>
+  <template v-else>
+    <DayoffListTable/>
+  </template>
 </template>
 
 <script>
 import ClickableRowTable from "@/components/table/ClickableTable.vue";
 import DayoffListTable from "@/views/dayoff/DayoffListTable.vue";
+import Calendar from "@/views/calendar/calendar.vue";
 export default {
   name: 'DayoffListView',
-  components: {DayoffListTable, ClickableRowTable},
-
+  components: {Calendar, DayoffListTable, ClickableRowTable},
+  methods: {
+    changeCalendar() {
+      this.isCalendarView = true;
+    },
+    changeList() {
+      this.isCalendarView = false;
+    }
+  },
+  data() {
+    return {
+      isCalendarView : true,
+    }
+  }
 }
 </script>
 
